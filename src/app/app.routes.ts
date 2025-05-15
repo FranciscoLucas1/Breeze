7d1506bb7a2e32eb31d14ae230c316c5b670b331
FranciscoLucas1 <franciscoolucas@hotmail.com>
2025-05-15 16:22:45 -0300

    Alterações visuais no login e configurações de roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: LoginComponent},
+    {path: "cadastro", component: CadastroComponent},
+    {path: "inicio", component: InicioComponent},
+    {path: "perfil", component: PerfilComponent},
+
+];
